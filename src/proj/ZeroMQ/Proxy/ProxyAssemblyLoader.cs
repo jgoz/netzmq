601426a48b086ff06b5c4c0c50cd08e12253719f
Johnny Gozde <johnny@jgoz.net>
2011-09-19 00:29:34 -0600

    Add dependency loading via manifest resources.
    Dependency path precedence is as follows: bin, <current execution path>,
    <temporary directory>. If the dependencies exist at any of these paths, they
    will be used. Otherwise, they will be extracted to the first writable path
    and loaded from there.

---
+++ src/proj/ZeroMQ/Proxy/ProxyAssemblyLoader.cs
@@ -0,0 +1,107 @@
+﻿namespace ZeroMQ.Proxy
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+
+    internal static class ProxyAssemblyLoader
+    {
+        private const string ProxyAssemblyFileName = "ZeroMQ.Proxy.dll";
+        private const string NativeDllName = "libzmq.dll";
+        private const string LoadError = "Unable to load {0} for {1} platform.";
+        private const string WriteAccessRequired = " Write access to the current executing path or a temporary folder is required.";
+
+        private static readonly string CurrentPlatform = Environment.Is64BitProcess ? "x64" : "x86";
+
+        public static Assembly Load()
+        {
+            Assembly assembly = LoadFromLocalBinPath() ?? LoadFromExecutingPath() ?? LoadFromTempPath();
+
+            if (assembly == null)
+            {
+                throw new InvalidOperationException(string.Format(LoadError, ProxyAssemblyFileName, CurrentPlatform) + WriteAccessRequired);
+            }
+
+            return assembly;
+        }
+
+        private static Assembly LoadFromLocalBinPath()
+        {
+            return ExtractAndLoadFromPath("bin");
+        }
+
+        private static Assembly LoadFromExecutingPath()
+        {
+            return ExtractAndLoadFromPath(".");
+        }
+
+        private static Assembly LoadFromTempPath()
+        {
+            string dir = Path.Combine(Path.GetTempPath(), "netzmq", CurrentPlatform);
+            Directory.CreateDirectory(dir);
+
+            return ExtractAndLoadFromPath(dir);
+        }
+
+        private static Assembly ExtractAndLoadFromPath(string dir)
+        {
+            if (!Directory.Exists(dir))
+            {
+                return null;
+            }
+
+            string proxyPath = Path.GetFullPath(Path.Combine(dir, ProxyAssemblyFileName));
+            string nativePath = Path.GetFullPath(Path.Combine(dir, NativeDllName));
+            string platformSuffix = "." + CurrentPlatform;
+
+            if (!ExtractManifestResource(NativeDllName + platformSuffix, nativePath))
+            {
+                return null;
+            }
+
+            if (!ExtractManifestResource(ProxyAssemblyFileName + platformSuffix, proxyPath))
+            {
+                return null;
+            }
+
+            return Assembly.LoadFrom(proxyPath);
+        }
+
+        private static bool ExtractManifestResource(string resourceName, string outputPath)
+        {
+            if (File.Exists(outputPath))
+            {
+                // There is potential for version conflicts by doing this. However, it is necessary to
+                // prevent file access conflicts if multiple processes are run from the same location.
+
+                // TODO: Implement a better file conflict prevention scheme
+                return true;
+            }
+
+            Stream resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+
+            if (resourceStream == null)
+            {
+                // No manifest resources were compiled into the current assembly. This is likely a 'manual
+                // deployment' situation, so do not throw an exception at this point and allow all deployment
+                // paths to be searched.
+                return false;
+            }
+
+            try
+            {
+                using (FileStream fileStream = File.Create(outputPath))
+                {
+                    resourceStream.CopyTo(fileStream);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // Caller does not have write permission for the current file
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- src/proj/ZeroMQ/Proxy/ProxyFactory.cs
+++ src/proj/ZeroMQ/Proxy/ProxyFactory.cs
@@ -1,32 +1,25 @@
 ﻿namespace ZeroMQ.Proxy
 {
     using System;
-    using System.IO;
     using System.Linq;
     using System.Reflection;
 
     internal static class ProxyFactory
     {
-        private const string InteropAssemblyName = "ZeroMQ.Proxy";
 
-        private static readonly Assembly NativeAssembly;
+        private static readonly Assembly ProxyAssembly;
 
         private static readonly Type SocketContextProxyType;
         private static readonly Type SocketProxyType;
 
         static ProxyFactory()
         {
-            NativeAssembly = LoadInteropAssembly();
+            ProxyAssembly = ProxyAssemblyLoader.Load();
 
-            if (NativeAssembly == null)
-            {
-                throw new FileNotFoundException("Unable to find suitable ZeroMQ.Proxy assembly.");
-            }
+            SocketContextProxyType = ProxyAssembly.GetTypes().Single(t => typeof(ISocketContextProxy).IsAssignableFrom(t));
+            SocketProxyType = ProxyAssembly.GetTypes().Single(t => typeof(ISocketProxy).IsAssignableFrom(t));
 
-            SocketContextProxyType = NativeAssembly.GetTypes().Single(t => typeof(ISocketContextProxy).IsAssignableFrom(t));
-            SocketProxyType = NativeAssembly.GetTypes().Single(t => typeof(ISocketProxy).IsAssignableFrom(t));
-
-            Type errorProviderType = NativeAssembly.GetTypes().Single(t => typeof(IErrorProviderProxy).IsAssignableFrom(t));
+            Type errorProviderType = ProxyAssembly.GetTypes().Single(t => typeof(IErrorProviderProxy).IsAssignableFrom(t));
             ErrorProvider = (IErrorProviderProxy)Activator.CreateInstance(errorProviderType);
         }
 
@@ -40,32 +33,6 @@
         public static ISocketProxy CreateSocket(IntPtr context, SocketType socketType)
         {
             return (ISocketProxy)Activator.CreateInstance(SocketProxyType, context, socketType);
-        }
-
-        private static Assembly LoadInteropAssembly()
-        {
-            AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly;
-
-            return Assembly.Load(InteropAssemblyName);
-        }
-
-        private static Assembly ResolveAssembly(object sender, ResolveEventArgs args)
-        {
-            if (!args.Name.StartsWith(InteropAssemblyName))
-            {
-                return Assembly.Load(args.Name);
-            }
-
-            // TODO: Try loading normally first, then write manifest resource to disk
-
-            string shortName = new AssemblyName(args.Name).Name;
-
-            return Assembly.LoadFrom(GetAssemblyPath(shortName));
-        }
-
-        private static string GetAssemblyPath(string shortName)
-        {
-            return Path.Combine(Environment.Is64BitProcess ? "x64" : "x86", shortName + ".dll");
         }
     }
 }
