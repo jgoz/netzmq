87c7987bcc3282803ff12363e7c7b85e11cba380
Johnny Gozde <johnny@jgoz.net>
2011-09-22 21:20:51 -0600

    Add IDuplexSocket interface.

---
+++ src/proj/ZeroMQ/IDuplexSocket.cs
@@ -0,0 +1,10 @@
+﻿namespace ZeroMQ
+{
+    /// <summary>
+    /// A socket that is capable of both sending and receiving messages to and from remote endpoints.
+    /// </summary>
+    public interface IDuplexSocket
+        : ISendSocket, IReceiveSocket
+    {
+    }
+}
