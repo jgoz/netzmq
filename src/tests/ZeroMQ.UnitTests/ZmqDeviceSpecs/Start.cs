c767540666928b7df4f537c9ae25f004b0731fa9
Johnny Gozde <johnny@jgoz.net>
2011-10-05 22:14:14 -0600

    Add failure-case device specs.

--- src/tests/ZeroMQ.UnitTests/ZmqDeviceSpecs/Start.cs
+++ src/tests/ZeroMQ.UnitTests/ZmqDeviceSpecs/Start.cs
@@ -1,8 +1,11 @@
 ﻿namespace ZeroMQ.UnitTests.ZmqDeviceSpecs
 {
+    using System;
+
     using Machine.Specifications;
 
     using ZeroMQ.Proxy;
+    using ZeroMQ.Sockets;
     using ZeroMQ.Sockets.Devices;
 
     [Subject(typeof(ZmqDevice))]
@@ -29,5 +32,47 @@
 
         It should_connect_the_backend_socket = () =>
             backendProxy.Verify(mock => mock.Connect("connect"));
+    }
+
+    [Subject(typeof(ZmqDevice))]
+    class when_the_context_is_terminated : using_mock_device_proxy
+    {
+        static Exception exception;
+
+        Establish context = () =>
+        {
+            deviceProxy.Setup(mock => mock.Run()).Returns(-1);
+            errorProviderProxy.Setup(mock => mock.GetErrorCode()).Returns((int)ErrorCode.Eterm);
+        };
+
+        Because of = () =>
+            exception = Catch.Exception(() => device.Start());
+
+        It should_not_fail = () =>
+            exception.ShouldBeNull();
+
+        It should_not_be_running = () =>
+            device.IsRunning.ShouldBeFalse();
+    }
+
+    [Subject(typeof(ZmqDevice))]
+    class when_a_fatal_error_occurs : using_mock_device_proxy
+    {
+        static Exception exception;
+
+        Establish context = () =>
+        {
+            deviceProxy.Setup(mock => mock.Run()).Returns(-1);
+            errorProviderProxy.Setup(mock => mock.GetErrorCode()).Returns((int)ErrorCode.Efault);
+        };
+
+        Because of = () =>
+            exception = Catch.Exception(() => device.Start());
+
+        It should_fail = () =>
+            exception.ShouldBeOfType<ZmqLibException>();
+
+        It should_not_be_running = () =>
+            device.IsRunning.ShouldBeFalse();
     }
 }
