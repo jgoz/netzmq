a4d39965d67eb1875847a0e9a3967007b438df25
Johnny Gozde <johnny@jgoz.net>
2011-10-13 22:07:53 -0600

    Run specs when using build script.

--- build.cmd
+++ build.cmd
@@ -4,5 +4,5 @@ set DEPS_CMD=deps.cmd
 
 if not exist %PACKAGE_DIR% call %DEPS_CMD%
 
-%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild build.proj /target:Build /Property:Configuration=Release
+%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild build.proj /target:Build /Property:Configuration=Debug
 if errorlevel 1 pause else exit
