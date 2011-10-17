122f9e604d3daff3766742a90a70b691489aeefc
John Gozde <johnny@jgoz.net>
2011-10-16 18:59:47 -0600

    Add nuget package creation.

--- package.cmd
+++ package.cmd
@@ -1,8 +1,43 @@
 @echo off
-set PACKAGE_DIR=lib\packages
-set DEPS_CMD=deps.cmd
+set DEPS_DIR=lib\packages
+set NUGET_EXE=bin\nuget\nuget.exe
+set NUSPEC=netzmq.nuspec
+set VERSION_INFO_CS=src\proj\VersionAssemblyInfo.cs
+set VERSION_INFO_CPP=src\proj\ZeroMQ.Proxy\VersionAssemblyInfo.cpp
 
-if not exist %PACKAGE_DIR% call %DEPS_CMD%
+set /p VERSION=Enter version (e.g. 1.0): 
+set /p BUILD=Enter a build (e.g. 11234): 
+set /p REVISION=Enter a revision (e.g. 7): 
+set /p MATURITY=Enter maturity (e.g. Alpha, Beta, RC, Release, etc.): 
+
+:: C# shared version info
+move %VERSION_INFO_CS% %VERSION_INFO_CS%.bak
+echo using System.Reflection; > %VERSION_INFO_CS%
+echo. >> %VERSION_INFO_CS%
+echo [assembly: AssemblyVersion("%VERSION%.0.0")] >> %VERSION_INFO_CS%
+echo [assembly: AssemblyFileVersion("%VERSION%.%BUILD%.%REVISION%")] >> %VERSION_INFO_CS%
+echo [assembly: AssemblyInformationalVersion("%VERSION%.%BUILD%.%REVISION% %MATURITY%")] >> %VERSION_INFO_CS%
+
+:: VC++ proxy version info
+move %VERSION_INFO_CPP% %VERSION_INFO_CPP%.bak
+echo #include "stdafx.h"; > %VERSION_INFO_CPP%
+echo. >> %VERSION_INFO_CPP%
+echo using namespace System::Reflection; >> %VERSION_INFO_CPP%
+echo. >> %VERSION_INFO_CPP%
+echo [assembly:AssemblyVersionAttribute("%VERSION%.0.0")]; >> %VERSION_INFO_CPP%
+echo [assembly:AssemblyFileVersionAttribute("%VERSION%.%BUILD%.%REVISION%")]; >> %VERSION_INFO_CPP%
+echo [assembly:AssemblyInformationalVersionAttribute("%VERSION%.%BUILD%.%REVISION% %MATURITY%")]; >> %VERSION_INFO_CPP%
+
+if not exist %DEPS_DIR% call deps.cmd
 
 %SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild build.proj /target:Package /Property:Configuration=Release
+
+%NUGET_EXE% Pack %NUSPEC% -Version %VERSION%.%REVISION% -OutputDirectory package
+
+:: Clean up
+move %VERSION_INFO_CS%.bak %VERSION_INFO_CS%
+move %VERSION_INFO_CPP%.bak %VERSION_INFO_CPP%
+
+rd /S /Q out
+
 if errorlevel 1 pause else exit
