0be28689c0b8334fd1a8bc03ab3c0ec0e6f4eaed
John Gozde <johnny@jgoz.net>
2011-10-16 18:18:59 -0600

    Add global and version assembly info files.

--- src/proj/ZeroMQ.Proxy/AssemblyInfo.cpp
+++ src/proj/ZeroMQ.Proxy/AssemblyInfo.cpp
@@ -20,19 +20,6 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
 
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the value or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-
-[assembly:AssemblyVersionAttribute("1.0.0.0")];
-
 [assembly:ComVisible(false)];
 
 [assembly:CLSCompliantAttribute(true)];
