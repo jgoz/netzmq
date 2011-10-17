aa0bb8c58fba1cb876acea61b2eeb95c0de4cd62
John Gozde <johnny@jgoz.net>
2011-10-16 19:33:03 -0600

    Sign final assembly when packaging.

--- package.cmd
+++ package.cmd
@@ -4,6 +4,7 @@ set NUGET_EXE=bin\nuget\nuget.exe
 set NUSPEC=netzmq.nuspec
 set VERSION_INFO_CS=src\proj\VersionAssemblyInfo.cs
 set VERSION_INFO_CPP=src\proj\ZeroMQ.Proxy\VersionAssemblyInfo.cpp
+set INTERNALS_INFO_CS=src\proj\InternalsAssemblyInfo.cs
 
 set /p VERSION=Enter version (e.g. 1.0): 
 set /p BUILD=Enter a build (e.g. 11234): 
@@ -28,6 +29,12 @@ echo [assembly:AssemblyVersionAttribute("%VERSION%.0.0")]; >> %VERSION_INFO_CPP%
 echo [assembly:AssemblyFileVersionAttribute("%VERSION%.%BUILD%.%REVISION%")]; >> %VERSION_INFO_CPP%
 echo [assembly:AssemblyInformationalVersionAttribute("%VERSION%.%BUILD%.%REVISION% %MATURITY%")]; >> %VERSION_INFO_CPP%
 
+:: InternalsVisibleTo assembly info
+move %INTERNALS_INFO_CS% %INTERNALS_INFO_CS%.bak
+echo using System.Runtime.CompilerServices; > %INTERNALS_INFO_CS%
+echo. >> %INTERNALS_INFO_CS%
+echo [assembly: InternalsVisibleTo("ZeroMQ.Proxy, PublicKeyToken=9078bbfa744a021a")] >> %INTERNALS_INFO_CS%
+
 if not exist %DEPS_DIR% call deps.cmd
 
 %SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild build.proj /target:Package /Property:Configuration=Release
@@ -37,6 +44,7 @@ if not exist %DEPS_DIR% call deps.cmd
 :: Clean up
 move %VERSION_INFO_CS%.bak %VERSION_INFO_CS%
 move %VERSION_INFO_CPP%.bak %VERSION_INFO_CPP%
+move %INTERNALS_INFO_CS%.bak %INTERNALS_INFO_CS%
 
 rd /S /Q out
 
